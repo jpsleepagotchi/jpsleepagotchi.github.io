2ab244b8a82f558b9581d779e7644388b2943839
jp <jp@sleepagotchi.com>
2025-02-24 04:41:55 +0900

    Create script.js

---
+++ support/script.js
@@ -0,0 +1,11 @@
+document.getElementById("bg").innerHTML = `
+        <div class="glass">
+            <h1>Sleepagotchi Support</h1>
+            <p>
+                下部のフォームからご質問を英語で入力してください。改行すると送信されます。<br>
+                ※ 入力しづらい事象については改善を検討しています。<br />
+                <a href="https://discord.com/invite/sleepagotchi?openExternalBrowser=1" target="_blank">Discord</a> や <a href="https://t.me/SleepagotchiSupport_bot" target="_blank">テレグラム</a>からも相談が可能です🙇<br />
+                ※ フォームが出ていない時は右下の青いボタンを押してください。
+            </p>
+        </div>
+`;
